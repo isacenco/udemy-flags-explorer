33adf471409988e97bd8c7660a5e77b688209e2b
Ghenadie Isacenco <isacenco@gmail.com>
2025-11-06 15:24:11 +0100

    Card View

--- FlagsExplorer/View/FlagsExplorerView.swift
+++ FlagsExplorer/View/FlagsExplorerView.swift
@@ -32,8 +32,84 @@ struct FlagsExplorerView: View {
                     .padding()
                 
                 Spacer()
+                
+                FlagsExplorerCard(name: "Europe", desctiption: "Diverse cultures and vast landscapes, symbolized by flags with stars, stripes and vibrant motifs.", isSelected: true)
+                
+                Spacer()
             }
         }
+    }
+}
+
+struct FlagsExplorerCard: View {
+    let name: String
+    let desctiption: String
+    let isSelected: Bool
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 60)
+                .fill(.white)
+                .shadow(radius: 10)
+                .frame(width: 200, height: 250)
+            
+            VStack {
+                Spacer()
+                
+                Image(name.lowercased())
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 180, height: 180)
+                    .shadow(radius: 20)
+                
+                Text(name)
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(.bg1)
+                    .shadow(radius: 10)
+                
+                Text(desctiption)
+                    .font(.system(size: 12))
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 8)
+                    .foregroundStyle(.gray)
+                    
+                if isSelected {
+                    NavigationLink {
+                        
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .fill(
+                                    LinearGradient(
+                                        colors: [
+                                            .yellow,
+                                            .yellow,
+                                            .orange
+                                        ],
+                                        startPoint: .topLeading,
+                                        endPoint: .bottomTrailing
+                                    )
+                                )
+                                .frame(width: 100, height: 100)
+                            
+                            Circle()
+                                .stroke(.white, lineWidth: 10)
+                                .frame(width: 100, height: 100)
+                            
+                            Image(systemName: "arrow.forward")
+                                .foregroundStyle(.white)
+                                .font(.system(size: 40, weight: .bold))
+                        }
+                    }
+                }
+                
+                
+
+            }
+            .offset(y: isSelected ? -130 : -230)
+        }
+        .frame(width: 200)
     }
 }
 
