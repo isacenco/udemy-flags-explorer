6f8643a4da18c7d095456f57f77de09f255a3b5b
Ghenadie Isacenco <isacenco@gmail.com>
2025-11-06 19:28:39 +0100

    Grid View

--- FlagsExplorer/View/FlagsDetailsView.swift
+++ FlagsExplorer/View/FlagsDetailsView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct FlagsDetailsView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State var flagsDetailViewModel: FlagsDetailViewModel = FlagsDetailViewModel()
+    
     var name: String
+    var columns: [GridItem] = [
+        GridItem(.flexible(minimum: 100, maximum: 200)),
+        GridItem(.flexible(minimum: 100, maximum: 200))
+    ]
     
     fileprivate func BackButton() -> ToolbarItem<(), Button<some View>> {
         return ToolbarItem(placement: .navigationBarLeading) {
             Button {
-                
+                dismiss()
             } label: {
                 HStack {
                     Image(systemName: "chevron.left")
@@ -31,7 +39,34 @@ struct FlagsDetailsView: View {
                 .ignoresSafeArea()
                 
             VStack {
-                Spacer()
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(flagsDetailViewModel.countries, id: \.id) { country in
+                            VStack {
+                                AsyncImage(url: URL(string: country.flags.png)) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                
+                                Text(country.name.common)
+                                    .font(.title3)
+                                    .foregroundStyle(.white)
+                            }
+                            .padding()
+                            .background(.ultraThinMaterial)
+                            .cornerRadius(10)
+                            .shadow(color: .gray.opacity(0.3), radius: 5, x: 0, y: 5)
+                            
+                        }
+                    }
+                    .task {
+                        await flagsDetailViewModel.getCountriesByContinent(continent: name)
+                    }
+                }
                 
                 NavigationLink {
                     
